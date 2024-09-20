db8610d10455b9214980af9676d8dbae39420cb8
zsw <1713260995@qq.com>
2024-09-20 22:52:38 +0800

    feat: 初始化项目

---
+++ ADXS-Server/ADXS-Server/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ADXS_Server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
